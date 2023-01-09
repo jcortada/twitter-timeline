c6fd4b9ac4d34d0effe777bde5dee519b010ac21
Cortada Coromina, Javier <jcortada@minsait.com>
2023-01-09 09:57:02 +0100

    update repository

--- devs4j-tweets-feed/src/main/java/com/devs4j/tweets/feed/repository/TweetRepository.java
+++ devs4j-tweets-feed/src/main/java/com/devs4j/tweets/feed/repository/TweetRepository.java
@@ -1,9 +1,9 @@
 package com.devs4j.tweets.feed.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.devs4j.tweets.feed.model.entity.TweetEntity;
 
-public interface TweetRepository extends CrudRepository<TweetEntity, String>{
+public interface TweetRepository extends MongoRepository<TweetEntity, String>{
 
 }
--- devs4j-twitter-accounts/src/main/java/com/devs4j/twitter/accounts/repository/AccountRepository.java
+++ devs4j-twitter-accounts/src/main/java/com/devs4j/twitter/accounts/repository/AccountRepository.java
@@ -1,11 +1,11 @@
 package com.devs4j.twitter.accounts.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.devs4j.twitter.accounts.model.entity.AccountEntity;
 
 @Repository
-public interface AccountRepository extends CrudRepository<AccountEntity, String>{
+public interface AccountRepository extends MongoRepository<AccountEntity, String>{
 
 }

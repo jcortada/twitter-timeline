8fe9dc60a97fea8c12ce2ef55b38bd3ed50379d8
Cortada Coromina, Javier <jcortada@minsait.com>
2023-01-16 18:17:21 +0100

    init redis project

---
+++ devs4j-twitter-redis/src/test/java/com/devs4j/twitter/redis/Devs4jTwitterRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devs4j.twitter.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devs4jTwitterRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
